b235839d294bea85b6f71b1ba5b47a44ab9e5d00
Noh_yg <skyt852@gmail.com>
2025-12-12 12:15:14 +0900

    💄헤더 퍼블리싱

--- src/components/new/WriteHeader.tsx
+++ src/components/new/WriteHeader.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 export default function WriteHeader() {
   return (
     <>
-      <header>
+      <header className="px-8 py-4">
         <div className="flex items-center gap-4">
           <Link href={"/dashboard"}>
             <ArrowLeft className="text-primary" />
           </Link>
           <div className="space-y-1">
-            <p className="text-2xl font-medium">
+            <p className="text-xl font-medium">
               편지 쓰기<span className="text-primary px-1">_</span>
             </p>
             <p className="text-text-3 text-sm">소중한 마음을 담아보세요</p>
