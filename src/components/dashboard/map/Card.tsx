a8074f18339d3e524f1c225a87256e9465095a8c
“moming109” <skhu202013029@gmail.com>
2025-12-18 12:29:21 +0900

    feat(map): 캡슐 title 검색 구현

--- src/components/dashboard/map/Card.tsx
+++ src/components/dashboard/map/Card.tsx
@@ -1,23 +1,49 @@
 import { MapPin } from "lucide-react";
+type CardProps = {
+  data: PublicCapsule;
+  keyword: string;
+};
 
-export default function Card({ data }: { data: PublicCapsule }) {
+export default function Card({ data, keyword }: CardProps) {
+  function highlightText(text: string, keyword: string) {
+    //키워드 없으면 반환
+    if (!keyword) return text;
+    const index = text.toLowerCase().indexOf(keyword.toLowerCase());
+    //키워드 일치하는 부분 없으면 반환
+    // if (index === -1) return text;
+
+    const before = text.slice(0, index);
+    const match = text.slice(index, index + keyword.length);
+    const after = text.slice(index + keyword.length);
+
+    return (
+      <>
+        {before}
+        <span className="text-primary-2 font-semibold">{match}</span>
+        {after}
+      </>
+    );
+  }
   return (
     <>
       <div className="border border-outline rounded-xl p-4 space-y-4">
         <div className="space-y-1">
-          <p>{data.title}</p>
+          <p>{highlightText(data.title, keyword)}</p>
           <div className="text-xs flex items-center gap-1 text-text-3">
             <div className="flex items-center gap-1">
-              <MapPin size={12} /> <span>서울시청</span>
+              <MapPin size={12} /> <span>장소 연결 X</span>
             </div>
-            <span>•</span> <span>{data.distanceToCapsule}m</span>
+            <span>•</span>
+            <span className="text-primary-2">
+              {Math.floor(data.distanceToCapsule)}m
+            </span>
           </div>
         </div>
         {/* <div className="text-sm">
           <p>벚꽃이 피는 이 거리에서 당신을 떠올립니다...</p>
         </div> */}
         <div className="flex justify-between text-xs text-text-2">
-          <span>by {data.writerNickname}</span>{" "}
+          <span>from. {data.writerNickname}</span>{" "}
           <span>{data.capsuleCreatedAt.slice(0, 10)}</span>
         </div>
       </div>
--- src/components/dashboard/map/MapList.tsx
+++ src/components/dashboard/map/MapList.tsx
@@ -10,9 +10,14 @@ type MapListProps = {
 export default function MapList({ listData }: MapListProps) {
   //검색 키워드
   const [keyword, setKeyword] = useState("");
+  const searchResultList = listData?.filter((d) => d.title.includes(keyword));
 
-  listData?.filter((d) => d.title.includes(keyword));
-  if (listData?.length === 0) return <p>주변에 편지가 없습니다.</p>;
+  if (listData?.length === 0)
+    return (
+      <p className="text-center text-text-4 py-8 text-sm">
+        주변에 편지가 없습니다.
+      </p>
+    );
   return (
     <>
       {/* 검색 */}
@@ -32,9 +37,15 @@ export default function MapList({ listData }: MapListProps) {
       {/* 리스트 스크롤 */}
       <div className="flex-1 min-h-0 overflow-y-auto space-y-3 px-6">
         {/* 리스트 1 */}
-        {listData?.map((d) => (
-          <Card key={d.capsuleId} data={d} />
-        ))}
+        {searchResultList?.length === 0 ? (
+          <p className="text-center text-text-4 py-8 text-sm">
+            일치하는 편지가 없습니다.
+          </p>
+        ) : (
+          searchResultList?.map((d) => (
+            <Card key={d.capsuleId} data={d} keyword={keyword} />
+          ))
+        )}
       </div>
     </>
   );
