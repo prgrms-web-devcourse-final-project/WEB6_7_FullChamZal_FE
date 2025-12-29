4715f29055c1024714ce1c14fd73f0e1bccbb451
NohYeonggwon <shdudrnjs0@naver.com>
2025-12-30 03:06:00 +0900

    feat: 내가 만든 트랙 리스트 조회

--- src/components/dashboard/storyTrack/mine/MineCard.tsx
+++ src/components/dashboard/storyTrack/mine/MineCard.tsx
@@ -1,46 +1,58 @@
-import { ListOrdered, MapPin, Users } from "lucide-react";
+import { ListOrdered, MapPin, Shuffle, Users } from "lucide-react";
+import Image from "next/image";
 
-export default function MineCard() {
+export default function MineCard({ track }: { track: StoryTrackJoinedItem }) {
   return (
     <>
       <div className="border border-outline rounded-xl">
         {/* Top */}
-        <div className="relative">
-          {/* 이미지로 변경 */}
-          <div className="w-full h-40 bg-amber-300 rounded-t-xl"></div>
-          {/* 순서o or 순서x */}
-          <div className="absolute top-3 right-3 flex items-center gap-1 px-3 py-2 rounded-lg text-white bg-admin text-sm">
-            <ListOrdered size={18} />
-            <span>순서대로</span>
-            {/* <Shuffle size={18} />
-            <span>순서 무관</span> */}
-          </div>
-        </div>
+        <Image
+          src="https://cdn.pixabay.com/photo/2024/01/15/21/13/puppy-8510899_1280.jpg"
+          alt={track.title}
+          width={800}
+          height={200}
+          className="w-full h-40 object-cover rounded-t-xl"
+        />
+
         {/* Bottom */}
         <div className="p-6 space-y-4">
           {/* 제목과 소개 */}
           <div className="space-y-1">
-            <p>성수동 카페투어</p>
+            <p>{track.title}</p>
             <p className="text-sm text-text-3 line-clamp-2 break-keep">
-              힙한 성수동의 숨은 카페들을 찾아가는 여정. 각 카페마다 특별한
-              이야기가 담겨있습니다.
+              {track.description}
             </p>
           </div>
 
           {/* 아이콘 요약 */}
           <div className="text-text-3 text-xs flex items-center gap-3">
             <div className="flex gap-1 items-center">
+              {track.trackType === "FREE" ? (
+                <>
+                  <Shuffle size={18} />
+                  순서 무관
+                </>
+              ) : (
+                <>
+                  <ListOrdered size={16} />
+                  순서대로
+                </>
+              )}
+            </div>
+            <div className="flex gap-1 items-center">
               <MapPin size={16} />
-              5개 장소
+              {track.totalSteps}개 장소
             </div>
             <div className="flex gap-1 items-center">
               <Users size={16} />
-              123명
+              {track.totalMemberCount}명
             </div>
           </div>
 
           {/* 생성일 */}
-          <div className="text-xs text-text-3">생성일: 2024. 12. 15.</div>
+          <div className="text-xs text-text-3">
+            생성일: {track.createdAt.slice(0, 10)}
+          </div>
         </div>
       </div>
     </>
