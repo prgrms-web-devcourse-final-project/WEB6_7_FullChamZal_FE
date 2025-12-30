56dc49c17ab05bb5243cd4ac042288c30d3f84da
NohYeonggwon <shdudrnjs0@naver.com>
2025-12-31 01:42:35 +0900

    fix: build

--- src/app/(auth)/auth/account-recovery/page.tsx
+++ src/app/(auth)/auth/account-recovery/page.tsx
@@ -1,5 +1,10 @@
 import AccountRecoveryPage from "@/components/auth/Account-recovery";
+import { Suspense } from "react";
 
 export default function Page() {
-  return <AccountRecoveryPage />;
+  return (
+    <Suspense fallback={<div className="p-8">로딩 중...</div>}>
+      <AccountRecoveryPage />
+    </Suspense>
+  );
 }
