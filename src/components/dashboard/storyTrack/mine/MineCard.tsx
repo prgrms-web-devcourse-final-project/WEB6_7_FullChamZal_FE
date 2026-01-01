4e3278394801f667f7609382b40791d6d2affd8e
NohYeonggwon <shdudrnjs0@naver.com>
2026-01-01 22:42:35 +0900

    feat: 내가 만든 트랙 카드 Link 연결

--- src/components/dashboard/storyTrack/mine/MineCard.tsx
+++ src/components/dashboard/storyTrack/mine/MineCard.tsx
@@ -1,60 +1,62 @@
 import { ListOrdered, MapPin, Shuffle, Users } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function MineCard({ track }: { track: StoryTrackJoinedItem }) {
   return (
-    <>
-      <div className="border border-outline rounded-xl">
-        {/* Top */}
-        <Image
-          src="https://cdn.pixabay.com/photo/2024/01/15/21/13/puppy-8510899_1280.jpg"
-          alt={track.title}
-          width={800}
-          height={200}
-          className="w-full h-40 object-cover rounded-t-xl"
-        />
+    <Link
+      href={`/dashboard/storyTrack/${track.storytrackId}`}
+      className="block border border-outline rounded-xl hover:shadow-md transition"
+    >
+      {/* Top */}
+      <Image
+        src="https://cdn.pixabay.com/photo/2024/01/15/21/13/puppy-8510899_1280.jpg"
+        alt={track.title}
+        width={800}
+        height={200}
+        className="w-full h-40 object-cover rounded-t-xl"
+      />
 
-        {/* Bottom */}
-        <div className="p-6 space-y-4">
-          {/* 제목과 소개 */}
-          <div className="space-y-1">
-            <p>{track.title}</p>
-            <p className="text-sm text-text-3 line-clamp-2 break-keep">
-              {track.description}
-            </p>
+      {/* Bottom */}
+      <div className="p-6 space-y-4">
+        {/* 제목과 소개 */}
+        <div className="space-y-1">
+          <p className="font-medium">{track.title}</p>
+          <p className="text-sm text-text-3 line-clamp-2 break-keep">
+            {track.description}
+          </p>
+        </div>
+
+        {/* 아이콘 요약 */}
+        <div className="text-text-3 text-xs flex items-center gap-3">
+          <div className="flex gap-1 items-center">
+            {track.trackType === "FREE" ? (
+              <>
+                <Shuffle size={18} />
+                순서 무관
+              </>
+            ) : (
+              <>
+                <ListOrdered size={16} />
+                순서대로
+              </>
+            )}
           </div>
-
-          {/* 아이콘 요약 */}
-          <div className="text-text-3 text-xs flex items-center gap-3">
-            <div className="flex gap-1 items-center">
-              {track.trackType === "FREE" ? (
-                <>
-                  <Shuffle size={18} />
-                  순서 무관
-                </>
-              ) : (
-                <>
-                  <ListOrdered size={16} />
-                  순서대로
-                </>
-              )}
-            </div>
-            <div className="flex gap-1 items-center">
-              <MapPin size={16} />
-              {track.totalSteps}개 장소
-            </div>
-            <div className="flex gap-1 items-center">
-              <Users size={16} />
-              {track.totalMemberCount}명
-            </div>
+          <div className="flex gap-1 items-center">
+            <MapPin size={16} />
+            {track.totalSteps}개 장소
           </div>
-
-          {/* 생성일 */}
-          <div className="text-xs text-text-3">
-            생성일: {track.createdAt.slice(0, 10)}
+          <div className="flex gap-1 items-center">
+            <Users size={16} />
+            {track.totalMemberCount}명
           </div>
         </div>
+
+        {/* 생성일 */}
+        <div className="text-xs text-text-3">
+          생성일: {track.createdAt.slice(0, 10)}
+        </div>
       </div>
-    </>
+    </Link>
   );
 }
