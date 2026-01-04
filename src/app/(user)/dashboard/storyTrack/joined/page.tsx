d0dd98319cffeb7ff3fdd40ef277bb64834245fa
NohYeonggwon <shdudrnjs0@naver.com>
2026-01-04 19:07:45 +0900

    fix: 스켈레톤 ui 추가

--- src/app/(user)/dashboard/storyTrack/joined/page.tsx
+++ src/app/(user)/dashboard/storyTrack/joined/page.tsx
@@ -1,10 +1,11 @@
 import JoinedTrackPage from "@/components/dashboard/storyTrack/joined/JoinedTrackPage";
+import StorytrackPageSkeleton from "@/components/skeleton/dashboard/storytrack/StorytrackPageSkeleton";
 import { Suspense } from "react";
 
 export default function JoinedPage() {
   return (
     <>
-      <Suspense fallback={<div className="p-8">로딩 중...</div>}>
+      <Suspense fallback={<StorytrackPageSkeleton />}>
         <JoinedTrackPage />
       </Suspense>
     </>
