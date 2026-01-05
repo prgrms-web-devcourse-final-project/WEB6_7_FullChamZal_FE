15daf6f967998c52c04e718de10fd7266b81a850
NohYeonggwon <shdudrnjs0@naver.com>
2026-01-05 16:30:11 +0900

    fix: 정식 위치로 수정

--- next.config.ts
+++ next.config.ts
@@ -10,10 +10,7 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  experimental: {
-    // @ts-expect-error - 실험적 단계라 아직 타입 정의에 포함되지 않음
-    reactCompiler: true,
-  },
+  reactCompiler: true,
 };
 
 export default nextConfig;
